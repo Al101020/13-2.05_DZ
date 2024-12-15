ab6f9ec7674af144eef0e55ecef5f265b1e3bcca
Al101020 <alex.vl.m@yandex.ru>
2024-12-15 23:27:46 +0300

    commit-1.1 button

---
+++ src/components/popovers/button.js
@@ -0,0 +1,6 @@
+const divPopovers = document.querySelector('.popovers');
+
+const btnPopovers = document.createElement('button');
+btnPopovers.className = 'btnPopovers';
+btnPopovers.innerHTML = 'Click to toggle popover';
+divPopovers.appendChild(btnPopovers);
---
+++ src/components/popovers/popovers.js
@@ -0,0 +1,3 @@
+import './button';
+import './tooltip';
+import './popovers.css';
