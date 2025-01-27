7197cbd4e1908f411cc61a3a1fabcb0ea9ed48f0
Al101020 <alex.vl.m@yandex.ru>
2025-01-27 13:43:02 +1000

    commit-2.4

--- e2e/start_.test.js
+++ e2e/start_.test.js
@@ -1,5 +1,7 @@
 import puppeteer from 'puppeteer';
 
+jest.setTimeout(30000); // default puppeteer timeout
+
 describe('Page start', () => {
   let browser;
   let page;
@@ -11,16 +13,23 @@ describe('Page start', () => {
       devtools: true,
     });
 
-    page = await browser.newPage();
+    page = await browser.newPage(); // открыли браузер и новую вкладку
   });
 
   test('test', async () => {
-    await page.goto('http://localhost:9000/');
+    await page.goto('http://localhost:9000/'); // проверили открывается ли путь
 
-    await page.waitFor('body');
+    // await page.waitFor('body'); // утарел метод - Метод waitFor недействующий
+    await page.waitForSelector('body'); // проверили есть ли тег "body"
+
+    const popovers = await page.$('.popovers');
+    const btn = await popovers.$('.btnPopovers');
+
+    await btn.click(); //  клик на кнопке
+    await page.waitForSelector('.messagePopovers'); // проверили есть ли тег с классом "messagePopovers"
   });
 
   afterAll(async () => {
-    await browser.close();
+    await browser.close(); // закрыли браузер
   });
 });
